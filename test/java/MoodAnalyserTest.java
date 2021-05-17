2aa391d21f172d5637b502075bb9c5db7cd8b3ba
snehalupatil <patilsnehal4575@gmail.com>
2021-05-17 11:42:48 +0530

    Added: The message is added

--- test/java/MoodAnalyserTest.java
+++ test/java/MoodAnalyserTest.java
@@ -1,12 +1,12 @@
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test
 
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_Whensad_shouldReturnSad()  {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is sad message");
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
 
         Assertions.assertEquals("SAD", mood);
     }
