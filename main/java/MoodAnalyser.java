984d0c415a6201c238154df566dd895f881341c9
snehalupatil <patilsnehal4575@gmail.com>
2021-05-17 11:43:57 +0530

    Added: This method return SAD

--- main/java/MoodAnalyser.java
+++ main/java/MoodAnalyser.java
@@ -3,7 +3,5 @@ public class MoodAnalyser {
     public String analyseMood(String message) {
         if (message.contains("sad"))
             return "SAD";
-        else
-            return "HAPPY";
     }
 }
